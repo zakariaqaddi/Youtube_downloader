47cb91fc69175c1f682acd6be049b4a3721b0c4d
zakariaqaddi <zakariaqaddi@gmail.com>
2020-11-12 21:39:39 +0100

    add icone

--- Interface.py
+++ Interface.py
@@ -85,7 +85,7 @@ def telecharger():
 top = Tk(className=' GUI YOUTUBE DOWNLOADER')
 # top['background'] = '#856ff8'
 top.geometry("450x250")
-top.iconbitmap(r'C:\Users\hamza\Desktop\Youtube_dw\icone.ico')
+top.iconbitmap(r'C:\Users\PC\Desktop\Youtube_downloader\youtube.ico')
 
 radio = IntVar()
 
